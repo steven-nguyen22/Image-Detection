659a1f5a701bf8c3b432130a489b7e297dfec48f
steven-nguyen22 <snguy117@ucr.edu>
2024-08-26 17:34:31 -0700

    chunk split

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), chunkSplitPlugin()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
